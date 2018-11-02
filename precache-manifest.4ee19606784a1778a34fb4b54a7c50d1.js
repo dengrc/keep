f0723789c3c708e1905f50481ceba9b34cab36d1
dengruchun <drc925@live.com>
2018-11-02 11:04:10 +0800

    service-worker url

--- precache-manifest.4ee19606784a1778a34fb4b54a7c50d1.js
+++ precache-manifest.4ee19606784a1778a34fb4b54a7c50d1.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   },
   {
     "revision": "8b46ba1838dba98291ff",
-    "url": "/js/chunk-vendors.972dddd6.js"
+    "url": "js/chunk-vendors.972dddd6.js"
   },
   {
     "revision": "a22fde34d07bdde97bd9",
-    "url": "/js/app.82f85d3d.js"
+    "url": "js/app.82f85d3d.js"
   },
   {
     "revision": "507b7aca07c1343fbc06a5cb9c2708d5",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "8b46ba1838dba98291ff",
-    "url": "/css/chunk-vendors.2b35904f.css"
+    "url": "css/chunk-vendors.2b35904f.css"
   },
   {
     "revision": "a22fde34d07bdde97bd9",
-    "url": "/css/app.98e66dc6.css"
+    "url": "css/app.98e66dc6.css"
   }
 ];
