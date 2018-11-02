7f381412c41550ebe060a98d5f2d22a8d02f9c85
dengruchun <drc925@live.com>
2018-11-02 10:24:05 +0800

    keep PWA

---
+++ precache-manifest.08510feda4f956662a5fc6e3b66abe7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8b46ba1838dba98291ff",
+    "url": "/js/chunk-vendors.972dddd6.js"
+  },
+  {
+    "revision": "21adc7cd645c4a2aff6f",
+    "url": "/js/app.6cb2e8df.js"
+  },
+  {
+    "revision": "bd7a87eff8369189ac2e372d59435ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b46ba1838dba98291ff",
+    "url": "/css/chunk-vendors.2b35904f.css"
+  },
+  {
+    "revision": "21adc7cd645c4a2aff6f",
+    "url": "/css/app.d63d7ef6.css"
+  }
+];
